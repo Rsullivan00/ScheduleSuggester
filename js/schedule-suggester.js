f425019e7b62aed9284ec850ae17aa852fee47a4
Rick Sullivan <rick@ricksullivan.net>
2014-10-30 18:34:08 -0700

    Merge fixed

--- js/schedule-suggester.js
+++ js/schedule-suggester.js
@@ -24,23 +24,6 @@ var COURSES = {
            new Course("AMTH 108 - Probability and Statistics", TYPES.mathScience),
            new Course("MATH 53 - Linear Algebra", TYPES.mathScience)],
     
-<<<<<<< HEAD
-    coen: [new Course("COEN 10 - Intro. to Programming", TYPES.engr),
-           new Course("COEN 11 - Advanced Programming", TYPES.engr),
-           new Course("COEN 12 - Data Structures", TYPES.engr),
-           new Course("COEN 19 - Discrete Math", TYPES.engr),
-           new Course("COEN 21 - Logic Design", TYPES.engr)],
-
-    chem:  new Course("CHEM 11 - Chemistry I", TYPES.mathScience),
-
-    phys: [new Course("PHYS 31 - Physics I", TYPES.mathScience),
-           new Course("PHYS 32 - Physics II", TYPES.mathScience),
-           new Course("PHYS 33 - Physics III", TYPES.mathScience)],
-
-    engr1: new Course("ENGR 1 - Intro. to Engineering", TYPES.engr),
-
-    candi: [ new Course("C&I 1 - Cultures & Ideas 1", TYPES.core),
-=======
     coen10: new Course("COEN 10 - Intro. to Programming", TYPES.engr),
     coen11: new Course("COEN 11 - Advanced Programming", TYPES.engr),
     coen12: new Course("COEN 12 - Data Structures", TYPES.engr),
@@ -56,31 +39,18 @@ var COURSES = {
     engr1: new Course("ENGR 1 - Intro. to Engineering", TYPES.engr),
 
     ci: [ new Course("C&I 1 - Cultures & Ideas 1", TYPES.core),
->>>>>>> fb6a3cc77e62c09d30a31469b79ac230ba4afd0f
            new Course("C&I 2 - Cultures & Ideas 2", TYPES.core)],
 
     ctw: [ new Course("CTW 1 - Critical Thinking & Writing I", TYPES.core),
            new Course("CTW 2 - Critical Thinking & Writing II", TYPES.core)],
-<<<<<<< HEAD
-=======
+
     core: new Course("University Core", TYPES.core),
->>>>>>> fb6a3cc77e62c09d30a31469b79ac230ba4afd0f
 
     engrButton: new Course(ENGR_BUTTON_TEXT, "") 
 };
 
 var credits = {
-<<<<<<< HEAD
-    math: 0,
-    setMath: function(score) {this.math = Math.max(this.math, score); },
-    coen: 0,
-    setCoen: function(score) {this.coen = Math.max(this.coen, score); },
-    phys: 0,
-    setPhys: function(score) {this.phys = Math.max(this.phys, score); },
-    chem: 0,
-    setChem: function(score) {this.chem = Math.max(this.chem, score); }
-=======
-    math: function() { 
+   math: function() { 
 	var creds = 0;
 	if (this.math11) creds++;
 	if (this.math12) creds++;
@@ -101,32 +71,12 @@ var credits = {
     phys32: false,
     chem: 0,
     chem11: false,
->>>>>>> fb6a3cc77e62c09d30a31469b79ac230ba4afd0f
 };
 
 var gschedule;
 var isEngr1Fall = true;
 
 var getSchedule = function() { 
-<<<<<<< HEAD
-    gschedule = [
-        [COURSES.ctw[0], 
-            COURSES.math[credits.math], 
-            COURSES.chem, 
-            COURSES.coen[credits.coen], 
-        ],
-        [COURSES.ctw[1], 
-            COURSES.math[credits.math + 1], 
-            COURSES.phys[credits.phys], 
-            COURSES.coen[credits.coen + 1],
-        ],
-        [COURSES.coen[credits.coen + 2], 
-            COURSES.math[credits.math + 2], 
-            COURSES.phys[credits.phys + 1], 
-            COURSES.coen[credits.coen + 3]]
-    ];
-
-=======
     function coreOrCourse(credit, course) {
 	if (credit)
 	    return COURSES.core;
@@ -169,7 +119,6 @@ var getSchedule = function() {
 	}
     }
 
->>>>>>> fb6a3cc77e62c09d30a31469b79ac230ba4afd0f
     if (isEngr1Fall) {
         gschedule[0].push(COURSES.engr1);
         gschedule[1].push(COURSES.engrButton);
@@ -208,87 +157,62 @@ var switchEngr1 = function() {
 };
 
 var SELECTS = {
-<<<<<<< HEAD
-    calcAB: function() { 
-        credits.setMath(1); 
-    },
-    calcBC: function() { 
-        credits.setMath(2); 
-    },
-    csci:   function() { 
-        credits.setCoen(1); 
-    },
-    chem:   function() { 
-        credits.setChem(1); 
-    },
-    physM:  function() { 
-        credits.setPhys(1); 
-    },
-    /* TODO: Needs to be changed to account for skipping different classes */
-    physEM: function() { 
-        credits.setPhys(1); 
-=======
     calcAB: function(score) { 
-	if (score > 3) {
-	    credits.math11 = true;
-	} else {
-	    credits.math11 = false;
-	}
+        if (score > 3) {
+            credits.math11 = true;
+        } else {
+            credits.math11 = false;
+        }
     },
     calcBC: function(score) { 
-	/* TODO: Fix bug with calcAB and BC undoing */
-	if (score == 3) {
-	    credits.math11 = true; 
-	} else if (score > 3) {
-	    credits.math11 = true; 
-	    credits.math12 = true; 
-	} else {
-	    credits.math11 = false;
-	    credits.math12 = false;
-	}
+        /* TODO: Fix bug with calcAB and BC undoing */
+        if (score == 3) {
+            credits.math11 = true; 
+        } else if (score > 3) {
+            credits.math11 = true; 
+            credits.math12 = true; 
+        } else {
+            credits.math11 = false;
+            credits.math12 = false;
+        }
     },
     csci:   function(score) { 
-	if (score > 3) {
-	    credits.coen11 = true; 
-	} else {
-	    credits.coen11 = false; 
-	}
+        if (score > 3) {
+            credits.coen11 = true; 
+        } else {
+            credits.coen11 = false; 
+        }
     },
     chem:   function(score) { 
-	if (score > 2) {
-	    credits.chem11 = true; 
-	} else {
-	    credits.chem11 = false; 
-	}
+        if (score > 2) {
+            credits.chem11 = true; 
+        } else {
+            credits.chem11 = false; 
+        }
     },
     physM:  function(score) { 
-	if (score > 3) {
-	    credits.phys31 = true; 
-	} else {
-	    credits.phys31 = false; 
-	}
+        if (score > 3) {
+            credits.phys31 = true; 
+        } else {
+            credits.phys31 = false; 
+        }
     },
     physEM: function(score) { 
         credits.setPhys(1); 
-	if (score > 4) {
-	    credits.phys33 = true; 
-	} else {
-	    credits.phys33 = false; 
-	}
->>>>>>> fb6a3cc77e62c09d30a31469b79ac230ba4afd0f
+        if (score > 4) {
+            credits.phys33 = true; 
+        } else {
+            credits.phys33 = false; 
+        }
     }
 };
 
 var selectChange = function(select) {
-<<<<<<< HEAD
-    SELECTS[$(select).attr('id')]($(select).val());
-=======
-    var val = $(select).val();
+   var val = $(select).val();
     /* N/A selected corresponds to a score of 0 */
     if (val === "N/A") 
 	val = 0;
     SELECTS[$(select).attr('id')](val);
->>>>>>> fb6a3cc77e62c09d30a31469b79ac230ba4afd0f
     drawSchedule();
 }
 
@@ -305,11 +229,8 @@ $(document).ready(function() {
 
     /* Need to account for deselecting (same for SELECT elements above) */
     $('#progExperience').on('change', function() {
-<<<<<<< HEAD
-        credits.setCoen(1);
-=======
+
         credits.coen11 = true;
->>>>>>> fb6a3cc77e62c09d30a31469b79ac230ba4afd0f
         drawSchedule(); 
     });
 
